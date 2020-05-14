f359641b5f149c4725e12d0016854400f91ac919
user <user@this.com>
2020-05-13 23:51:31 -0400

    add

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -3,4 +3,4 @@ void before(context){
 	println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep({ context.step in config.afterSteps })
+@AfterStep({ context.step.equals("static_code_analysis") })
